7470a1154753de998f323a32bc282587a18e143b
acidclouds <acid@cloud.sx>
2025-03-09 12:51:29 +0100

    Tmux: splits will use current path, wezterm supress glyph warnings

--- .wezterm.lua
+++ .wezterm.lua
@@ -9,6 +9,7 @@ local config = wezterm.config_builder()
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha"
 -- config.allow_square_glyphs_to_overflow_width = "Always"
+config.warn_about_missing_glyphs = false
 config.font = wezterm.font_with_fallback({ "IosevkaTerm Nerd Font Mono" })
 config.font_size = 14
 config.enable_tab_bar = false
