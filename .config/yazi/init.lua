7bd067e1efd0d97a67b3ebecbeffa703921e0732
acidclouds <acid@cloud.sx>
2025-04-15 14:14:22 +0200

    Combine Yazi config

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -12,3 +12,26 @@ require("starship"):setup({
 	-- Custom starship configuration file to use
 	config_file = "~/.config/starship.toml", -- Default: nil
 })
+
+require("augment-command"):setup({
+	prompt = true,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = true,
+	smart_paste = false,
+	smart_tab_create = false,
+	smart_tab_switch = false,
+	confirm_on_quit = false,
+	open_file_after_creation = false,
+	enter_directory_after_creation = false,
+	use_default_create_behaviour = false,
+	enter_archives = true,
+	extract_retries = 3,
+	recursively_extract_archives = true,
+	preserve_file_permissions = false,
+	must_have_hovered_item = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	smooth_scrolling = false,
+	scroll_delay = 0.02,
+	wraparound_file_navigation = false,
+})
