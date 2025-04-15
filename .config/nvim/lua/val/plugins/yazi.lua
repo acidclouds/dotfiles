147916c332a273e5b93b6b4e7120d483ebed5fd7
acidclouds <acid@cloud.sx>
2025-04-15 19:13:27 +0200

    Suda auto open, nvim yazi snacks.picker integration

--- .config/nvim/lua/val/plugins/yazi.lua
+++ .config/nvim/lua/val/plugins/yazi.lua
@@ -33,6 +33,10 @@ return {
 		keymaps = {
 			show_help = "<f1>",
 		},
+		integrations = {
+			grep_in_selected_files = "snacks.picker",
+			grep_in_directory = "snacks.picker",
+		},
 	},
 	-- 👇 if you use `open_for_directories=true`, this is recommended
 	init = function()
