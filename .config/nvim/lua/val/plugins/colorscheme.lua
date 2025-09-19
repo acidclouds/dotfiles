b0fb398302817c58c465d12640d4b3c02f6f90a1
Val <acid@cloud.sx>
2025-09-19 15:54:44 +0200

    power-profiles-deamon+nvim-colorscheme-update

--- .config/nvim/lua/val/plugins/colorscheme.lua
+++ .config/nvim/lua/val/plugins/colorscheme.lua
@@ -1,107 +1,113 @@
 return {
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					light = "latte",
-					dark = "mocha",
-				},
-				transparent_background = true, -- disables setting the background color.
-				show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-				term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-				dim_inactive = {
-					enabled = false, -- dims the background color of inactive window
-					shade = "dark",
-					percentage = 0.15, -- percentage of the shade to apply to the inactive window
-				},
-				no_italic = false, -- Force no italic
-				no_bold = false, -- Force no bold
-				no_underline = false, -- Force no underline
-				styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-					comments = { "italic" }, -- Change the style of comments
-					conditionals = { "italic" },
-					loops = {},
-					functions = {},
-					keywords = {},
-					strings = {},
-					variables = {},
-					numbers = {},
-					booleans = {},
-					properties = {},
-					types = {},
-					operators = {},
-					-- miscs = {}, -- Uncomment to turn off hard-coded styles
-				},
-				color_overrides = {},
-				custom_highlights = function(colors)
-					local latte = require("catppuccin.palettes").get_palette("latte")
-					local frappe = require("catppuccin.palettes").get_palette("frappe")
-					local macchiato = require("catppuccin.palettes").get_palette("macchiato")
-					local mocha = require("catppuccin.palettes").get_palette("mocha")
-					return {
-						LineNr = { fg = "#6C7086" },
-						-- LineNr = { fg = macchiato.overlay1 },
-						CursorLineNr = { fg = colors.mauve },
-						-- CursorLine = { bg = mocha.surface0 },
-					}
-				end,
-				-- custom_highlights = {},
-				default_integrations = true,
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					treesitter_context = true,
-					which_key = true,
-					notify = true,
-					noice = true,
-					mason = true,
-					alpha = true,
-					snacks = true,
-					nvim_surround = true,
-					lsp_trouble = true,
-					telescope = {
-						enabled = true,
-					},
-					mini = {
-						enabled = true,
-						indentscope_color = "",
-					},
-					native_lsp = {
-						enabled = true,
-						virtual_text = {
-							errors = { "italic" },
-							hints = { "italic" },
-							warnings = { "italic" },
-							information = { "italic" },
-							ok = { "italic" },
-						},
-						underlines = {
-							errors = { "underline" },
-							hints = { "underline" },
-							warnings = { "underline" },
-							information = { "underline" },
-							ok = { "underline" },
-						},
-						inlay_hints = {
-							background = true,
-						},
-					},
-					navic = {
-						enabled = true,
-						custom_bg = "NONE", -- "lualine" will set background to mantle
-					},
-					-- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-				},
-			})
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha", -- latte, frappe, macchiato, mocha
+        background = {     -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true, -- disables setting the background color.
+        float = {
+          transparent = true,          -- enable transparent floating windows
+          solid = false,               -- use solid styling for floating windows, see |winborder|
+        },
+        show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
+        -- term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false,             -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.15,           -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false,             -- Force no italic
+        no_bold = false,               -- Force no bold
+        no_underline = false,          -- Force no underline
+        -- float = true,
+        styles = {                     -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { "italic" },     -- Change the style of comments
+          conditionals = { "italic" },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = function(colors)
+          local latte = require("catppuccin.palettes").get_palette("latte")
+          local frappe = require("catppuccin.palettes").get_palette("frappe")
+          local macchiato = require("catppuccin.palettes").get_palette("macchiato")
+          local mocha = require("catppuccin.palettes").get_palette("mocha")
+          return {
+            LineNr = { fg = "#6C7086" },
+            -- LineNr = { fg = macchiato.overlay1 },
+            CursorLineNr = { fg = colors.mauve },
+            -- CursorLine = { bg = mocha.surface0 },
+          }
+        end,
+        -- custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          treesitter_context = true,
+          which_key = true,
+          notify = true,
+          noice = true,
+          mason = true,
+          alpha = true,
+          snacks = true,
+          nvim_surround = true,
+          lsp_trouble = true,
+          telescope = {
+            enabled = true,
+          },
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+              ok = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+              ok = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          navic = {
+            enabled = true,
+            custom_bg = "NONE", -- "lualine" will set background to mantle
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
 
-			-- setup must be called before loading
-			vim.cmd.colorscheme("catppuccin")
-		end,
-	},
+      -- setup must be called before loading
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
 }
